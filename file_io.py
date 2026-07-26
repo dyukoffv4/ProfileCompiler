d3bb0cd3f064dcd68b3a4400bb4b16bb1d0899bf
dyukoffv4 <net998art@gmail.com>
2026-07-26 21:58:27 +0300

    New io

--- file_io.py
+++ file_io.py
@@ -1,6 +1,9 @@
 """Функции чтения и записи текстовых файлов."""
 
 from pathlib import Path
+from typing import Any
+import json
+import csv
 
 
 def load_text(path: str | Path) -> str:
@@ -8,6 +11,35 @@ def load_text(path: str | Path) -> str:
     return Path(path).read_text(encoding="utf-8")
 
 
+def load_json(path: str | Path) -> Any:
+    """Прочитать JSON файл и вернуть его в виде объекта."""
+    return json.loads(Path(path).read_text(encoding="utf-8"))
+
+
+def load_csv(path: str | Path, *, delimiter: str = ",") -> list[list[str]]:
+    """Прочитать CSV-файл как список."""
+    with Path(path).open("r", encoding="utf-8", newline="") as file:
+        return [i for i in csv.reader(file, delimiter=delimiter)]
+
+
 def save_text(path: str | Path, data: str) -> None:
     """Записать строку в текстовый файл."""
-    Path(path).write_text(data, encoding="utf-8")
+    path = Path(path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text(data, encoding="utf-8")
+
+
+def save_json(path: str | Path, data: Any) -> None:
+    """Записать объект в текстовый файл в формате JSON."""
+    path = Path(path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text(json.dumps(data, indent=4), encoding="utf-8")
+
+
+def save_csv(path: str | Path, rows: list[list[str]], *, delimiter: str = ",") -> None:
+    """Записать список в CSV-файл."""
+    path = Path(path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with path.open("w", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file, delimiter=delimiter)
+        writer.writerows(rows)
